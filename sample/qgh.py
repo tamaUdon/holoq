accf62bbc92241f21e1a6ebad82a1e0a4f1a7acb
tamaUdon <sakurayuki603@gmail.com>
2025-11-28 18:55:49 +0900

    Wip

--- sample/qgh.py
+++ sample/qgh.py
@@ -5,6 +5,7 @@ import open3d.data
 import open3d as o3d
 import numpy as np
 import matplotlib.pyplot as plt
+from pointcloud import Constants
 
 
 def load_classical_pointcloud() -> open3d.geometry.PointCloud:
@@ -12,6 +13,39 @@ def load_classical_pointcloud() -> open3d.geometry.PointCloud:
     point_cloud = o3d.io.read_point_cloud(bunny_path)
     print("Loading data completed!")
     return point_cloud
+
+
+def calculate_bipolar_holography(data: np.ndarray, constants: Constants) -> np.ndarray:
+    """
+    calculate_bipolar_holography の Docstring
+
+    :param data: 点群データ
+    :type data: np.ndarray
+    :param constants: 定数データクラス
+    :type constants: Constants
+    :return: bipolarホログラムの計算結果
+    :rtype: np.ndarray
+    """
+
+    I_holography = np.zeros((constants.Y, constants.X))
+
+    print("Calculating CGH...")
+    for y_i in tqdm.tqdm(range(constants.Y)):
+        for x_i in range(constants.X):
+            for d in data:
+                x_j = d[0]
+                y_j = d[1]
+                z_j = d[2]
+
+                x_p = ((constants.PXL * x_i) - x_j) ** 2
+                y_p = ((constants.PXL * y_i) - y_j) ** 2
+                z_p = z_j**2
+
+                r = math.sqrt((x_p + y_p + z_p))  # TODO - Bipolarに変更する
+                I_tmp = (1 / r) * math.cos(constants.k * r)
+                I_holography[y_i, x_i] = I_tmp
+    print("CGH Calculation completed!")
+    return I_holography
 
 
 def preprocess_qbit_from_pointcloud(): ...
