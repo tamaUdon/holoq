488cf3c8ff8e8cb38952ba47e976b81ee0bd07f9
tamaUdon <sakurayuki603@gmail.com>
2025-12-18 20:29:37 +0900

    Wip - 複数点のゾーンプレート足し合わせ、ホログラムの再生コードの骨格を作成

---
+++ sample/sum_plates.py
@@ -0,0 +1,43 @@
+from sample.pointcloud import (
+    calculate_zoneplate,
+    create_single_point,
+    show_graph,
+    Constants,
+)
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+points_n = 2
+
+
+def sumup_plates(plates: list):
+    pass
+
+
+def main():
+    start = time.time()
+    print("Preparing for CGH...")
+
+    constants = Constants()
+    points = np.array([[0, 0, 0]])
+    hologram = np.array()
+
+    points = create_single_point(constants)
+
+    for n in range(points_n):
+        plate = calculate_zoneplate(points, constants)
+        hologram.additive(plate)  # 畳み込み
+
+    end = time.time()
+    print(print("Cal time:{} sec".format(end - start)))
+    show_graph(hologram)
+
+
+if __name__ == "__main__":
+    main()
+
+# TODO
+# 1. 畳み込み
+# 2. パディング
+# 3. デバッグ（正しく足し合わされているか）
