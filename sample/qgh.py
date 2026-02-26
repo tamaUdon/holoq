a06de49bb0ab84f8730ce8f4cf6645dab6fae26d
tamaUdon <sakurayuki603@gmail.com>
2026-02-26 23:26:49 +0900

    Wip - init_superposition_state()の実装

--- sample/qgh.py
+++ sample/qgh.py
@@ -1,132 +1,54 @@
 # 点群法で量子コンピュータ生成ホログラムを作る
 
 import time
-import math
 import tqdm
+import math
 import numpy as np
 import matplotlib.pyplot as plt
-from dataclasses import dataclass
 from typing import Iterable, Tuple
 from constants import Constants
-from pointcloud import (
-    create_single_point,
-    create_rectangle_points,
-    generate_hologram,
-    show,
-)
 from constants import Constants
 
 # Qiskit (Aer)
-from qiskit import ClassicalRegister, QuantumCircuit, QuantumRegister
+from qiskit import QuantumCircuit
 from qiskit_aer import AerSimulator
+from qiskit.circuit import QuantumRegister
+from qiskit.circuit.library import DraperQFTAdder, MultiplierGate, QFTGate, QFT
 
 bits_width = Constants.bits_w
 
 
-def prepare_basis_state(n: int, k: int) -> QuantumCircuit:
-    """
-    基底状態を作る
-
-    :param n: 量子ビットの数
-    :type n: int
-    :param k: 初期状態の値
-    :type k: int
-    :return: 量子回路
-    :rtype: QuantumCircuit
-    """
-    qc = QuantumCircuit(n)
-    for i in range(n):
-        if (k >> i) & 1:
-            qc.x(i)
-    return qc
+# 固定値
+N = 3
+a = [1, 2, 3, 0]
+ρ = [0.5, 0.25, 0.5, 0]
+xj_yj = [(0, 0), (1, 0), (0, 1), (1, 1)]
+xh_yh = [(0, 0), (1, 0), (0, 1), (1, 1)]
 
 
-# 古典点群法
-def monopolar_numpy(points: np.ndarray, constants: Constants):
-    # numpy実装版 - 512*512画素で7sec
-    x = np.arange(constants.X, dtype=np.float64) * constants.pp
-    y = np.arange(constants.Y, dtype=np.float64) * constants.pp
-    xx, yy = np.meshgrid(x, y)
-    hologram = np.zeros((constants.Y, constants.X), dtype=np.float64)
+def init_superposition_state():
+    # xh_start = 0  # 0-index
+    # xh_end = xh_start + bits_width  # |xh>
+    # yh_start = xh_end + 2  # |0>|0>
+    # yh_end = yh_start + bits_width  # |yh>
+    # rho_start = yh_end + 2  # |0>|0>
+    # total_bits_w = rho_start + bits_width + 1  # |ρj>|0>
 
-    for xj, yj, zj in tqdm.tqdm(points):
-        hx = xx - xj * constants.pp
-        hy = yy - yj * constants.pp
-        rho = constants.k / zj
-        phase = rho * (hx * hx + hy * hy + zj * zj)
-        hologram += np.where(np.cos(phase) >= 0.0, 1.0, -1.0)
+    xh_reg = QuantumRegister(bits_width, "xh")
+    anc1 = QuantumRegister(2, "anc1")  # |0>|0>
+    yh_reg = QuantumRegister(bits_width, "yh")
+    anc2 = QuantumRegister(2, "anc2")  # |0>|0>
+    rho_reg = QuantumRegister(bits_width, "rho")
+    anc3 = QuantumRegister(1, "anc3")  # |0>
 
-    return hologram
+    qc = QuantumCircuit(xh_reg, anc1, yh_reg, anc2, rho_reg, anc3)
 
+    sub = prepare_basis_state(bits_width, xh_value)
+    qc.compose(sub, qubits=xh_reg[:], inplace=True)
 
-# 量子ビットに埋め込み
-def encode_basis(points: np.ndarray, constants: Constants):
-    # 量子レジスタを初期化
-    a_qbit = ...
-    P_qbit = ...
-    x_qbit = ...
-    y_qbit = ...
+    # 値を入れたいレジスタ（xh, yh, rho）だけ prepare_basis_state() でXゲートをかける
 
-    for xj, yj, zj in tqdm.tqdm(points):
-        # xj, yj (zj) にアダマールゲートをかけて、重ね合わせ状態にする
-        Hadamrd_matrix()
-        ...
-        # a_j, rho_j, hx, hyにControlled-NOTをかけてqbitにし、重ね合わせ状態にする
-        CNOT()
-        ...
-
-    if len(points) % 2 != 0:
-        # 点群数が奇数ではないとき、2^2となる最小の数を求め、
-        # 不足しているビット数ぶん0（ダミー）を入れる
-        ...
-
-
-# アダマール行列
-# 重ね合わせをつくる行列
-Hadamrd_matrix = 1 / np.sqrt(2) * np.array([[1, 1], [1, -1]])
-
-# Controlled-Notゲート
-# 標的ビットを制御するゲート
-CNOT_matrix = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]])
-
-
-# Controlled-NOT
-def CNOT(): ...
-
-
-# Hadamard
-def H(): ...
-
-
-# 量子回路を作る
-ADD = ...
-MUL = ...
-SQR = ...
-F = ...
-F_1 = ...
-QFT = ...
-QFT_1 = ...
-
-
-def add(): ...
-
-
-def mul(): ...
-
-
-def sqr(): ...
-
-
-def f(): ...
-
-
-def f_inverse(): ...
-
-
-def qft(): ...
-
-
-def qft_inverse(): ...
+    ...
 
 
 # T(・)で測定
