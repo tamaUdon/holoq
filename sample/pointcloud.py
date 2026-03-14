65f80b29399c5e9da282621672df47e842a14ccc
tamaUdon <sakurayuki603@gmail.com>
2026-03-14 20:23:12 +0900

    Fix - 引数の型を最新に合わせる

--- sample/pointcloud.py
+++ sample/pointcloud.py
@@ -4,17 +4,17 @@ import time
 import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
-from constants import Constants
+from constants import ClassicalConstants
 
 
-def create_single_point(constants: Constants) -> np.ndarray:
+def create_single_point(constants: ClassicalConstants) -> np.ndarray:
     """
     create_single_point 1点の点群を作成する関数
 
     - X*Yの中心に物体点 (1点) がある想定
 
     :param constants: 定数クラスのオブジェクト
-    :type constants: Constants
+    :type constants: ClassicalConstants
     :return: デバッグ用の物体点 (1点)
     :rtype: np.ndarray
     """
@@ -25,14 +25,14 @@ def create_single_point(constants: Constants) -> np.ndarray:
     return np.array([[x0, y0, z0]], dtype=float)
 
 
-def create_small_opening(constants: Constants, width: int = 10) -> np.ndarray:
+def create_small_opening(constants: ClassicalConstants, width: int = 10) -> np.ndarray:
     """
     create_small_opening 小さな開口部の点群を作成する関数
 
     - X*Yの中心に物体点 (10*10点) がある想定
 
     :param constants: 定数クラスのオブジェクト
-    :type constants: Constants
+    :type constants: ClassicalConstants
     :param width: int
     :typewidth: 開口部の幅
     :return: デバッグ用の物体点 (1点)
@@ -53,12 +53,12 @@ def create_small_opening(constants: Constants, width: int = 10) -> np.ndarray:
     return points
 
 
-def create_four_points(constants: Constants) -> np.ndarray:
+def create_four_points(constants: ClassicalConstants) -> np.ndarray:
     """
     create_rect_points 4点の点群を作成する関数
 
     :param constants: 定数クラスのオブジェクト
-    :type constants: Constants
+    :type constants: ClassicalConstants
     """
 
     center = np.array([constants.X / 2, constants.Y / 2, constants.d])
@@ -74,12 +74,12 @@ def create_four_points(constants: Constants) -> np.ndarray:
     return center + signs * half
 
 
-def create_rectangle_points(constants: Constants) -> np.ndarray:
+def create_rectangle_points(constants: ClassicalConstants) -> np.ndarray:
     """
     create_rectangle_points 四角形の点群を作成する関数
 
     :param constants: 定数クラスのオブジェクト
-    :type constants: Constants
+    :type constants: ClassicalConstants
     """
 
     x_size = constants.X // 2  # Xの1/2サイズの四角形を作る
@@ -99,7 +99,7 @@ def create_rectangle_points(constants: Constants) -> np.ndarray:
     return rectangle
 
 
-def generate_hologram(points: np.ndarray, constants: Constants) -> np.ndarray:
+def generate_hologram(points: np.ndarray, constants: ClassicalConstants) -> np.ndarray:
     x = np.arange(constants.X, dtype=np.float64) * constants.pp
     y = np.arange(constants.Y, dtype=np.float64) * constants.pp
     xx, yy = np.meshgrid(x, y)
@@ -113,9 +113,9 @@ def generate_hologram(points: np.ndarray, constants: Constants) -> np.ndarray:
     return hologram
 
 
-def show(holography: np.ndarray) -> None:
+def show(holography: np.ndarray, X: int, Y: int) -> None:
     fig, ax = plt.subplots()
-    color = ax.contourf(range(Constants.X), range(Constants.Y), holography)
+    color = ax.contourf(range(X), range(Y), holography)
     fig.colorbar(color)
     fig.set_label("holography")
     plt.legend()
@@ -124,7 +124,7 @@ def show(holography: np.ndarray) -> None:
 
 def main():
     start = time.time()
-    constants = Constants()
+    constants = ClassicalConstants()
 
     points = create_rectangle_points(constants)  # 四角形 # TODO - 分岐
     hologram = generate_hologram(points, constants)
@@ -134,7 +134,7 @@ def main():
     print(print("Cal time:{} sec".format(end - start)))
 
     print("Preparing for display...")
-    show(hologram)
+    show(hologram, constants.X, constants.Y)
 
 
 if __name__ == "__main__":
