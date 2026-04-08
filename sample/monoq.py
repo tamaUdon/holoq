e205526b3ee369c68112e439f0fba7ba975f505d
mtcoffffee <mt.coffffee@gmail.com>
2026-04-08 22:00:08 +0900

    Wip - random()を実装

--- sample/monoq.py
+++ sample/monoq.py
@@ -4,12 +4,14 @@ import time
 from constants import ClassicalConstants
 from pointcloud import create_single_point, show
 from decimal import Decimal, getcontext, ROUND_HALF_UP
+
+
 # numpy を固定小数モードに
 np.set_printoptions(precision=16, floatmode='fixed', suppress=False)
 getcontext().prec = 16
 
-def monopolar_fixed_point(points: np.ndarray, constants: ClassicalConstants):
-    # Complex, amplitude and phase-only holograms using bipolar approximationのFig.2を参考に作成
+def monopolar_fixed_point(points: np.ndarray, constants: ClassicalConstants) -> np.ndarray:
+    # 1. fixed-point monopolar generated holography
 
     x = np.arange(constants.X, dtype=np.int64)
     y = np.arange(constants.Y, dtype=np.int64)
@@ -19,13 +21,10 @@ def monopolar_fixed_point(points: np.ndarray, constants: ClassicalConstants):
     p_sq = 2 * np.pi * constants.pp * constants.pp
     p_denom = constants.λ * constants.d
 
-    # 1. 固定小数monopolarを実装
-
-    N = p_sq / p_denom # M-bit 固定値なのでループの外側に出す
+    N = p_sq / p_denom # N-bit 固定値なのでループの外側に出す
     print(f"{p_sq=}")
     print(f"{p_denom=}")
-    print(f"{N=}") # N=0.0003038036213391664
-    #  TODO - ... order of nanometers, 𝑝2∕2𝜆𝑧𝑗 is a fractional value. If this is converted to 𝑀-bit fixed point format, the fixed-point is always positioned on the leftmost side
+    print(f"{N=}") 
 
     for i, (xj, yj, zj) in enumerate(tqdm.tqdm(points)):
         xhj = xh.astype(np.int32) - xj # hologram面を一気に計算
@@ -45,48 +44,51 @@ def monopolar_fixed_point(points: np.ndarray, constants: ClassicalConstants):
         θ = M * N # θ
         print(f"{θ=}")
 
-        # 1. fixed-point monopolar generated holography
         frac_part, int_part = np.modf(θ) # 例) 1.5 -> (0.5, 1.0)
         print(f"{frac_part=}")
         print(f"{int_part=}")
 
         decimal_arr = np.array([[Decimal(str(x).split(".")[1]) for x in row] for row in frac_part], dtype=object) # Decimal型に変換し、.以下をstrとして格納
-        
-        t_array = np.array([[(Decimal(str(int(x))[0])/10).quantize(Decimal('0'), ROUND_HALF_UP) for x in row] for row in decimal_arr], dtype=int) # 不格好なのでなおす
-        # decimal型 -> 0~9 整数が入ることはわかっている　-> 0~
-        # 四捨五入はok
-        print(f"{t_array=}")
- 
-        hologram = t_array  # t
+        hologram = np.array([[(Decimal(str(int(x))[0])/10).quantize(Decimal('0'), ROUND_HALF_UP) for x in row] for row in decimal_arr], dtype=int) # 不格好なのでなおす
+        # decimal型 -> 0~9 整数が入ることはわかっている　-> /10して四捨五入
+
         print(f"{hologram=}")
-        # hologram[i] = t.astype(int) # 足し合わせている -> 個別に保持する
-        # print(hologram[i])
-
-        # TODO - 2. calculate the ratio of 1
-        #  - T(・) ... 小数第一位を取得する関数を作り、1の数を数える
-        #    例) 75%などの確率を得る
-
-        # TODO - (3. calculate the ratio after measurement)
-        
-        # TODO - 3. hologram[] * measurement ratio 
 
     return hologram
 
+def measure(N: int, hologram: np.ndarray) -> tuple[float, float]:
+    count_one = np.count_nonzero(hologram == 1)
+    ratio_of_one = (1/N) * count_one
+    ratio_after_measure = 1/np.sqrt(N) / np.sqrt(ratio_of_one)
+
+    print(f"{ratio_of_one=}") # 0.5569496154785156
+    print(f"{ratio_after_measure=}")
+    return ratio_of_one, ratio_after_measure
+
+def random_hologram(ratio_of_one: float, hologram: np.ndarray, constants: ClassicalConstants) -> np.ndarray:
+    rng = np.random.default_rng()
+    random_filter = rng.random((constants.X, constants.Y))
+    bool_filter = (random_filter <= ratio_of_one)
+    return hologram & bool_filter
 
 def main():
-
     start = time.time()
 
     constants = ClassicalConstants()
     points = create_single_point(constants)
-    hologram = monopolar_fixed_point(points, constants)
+    hologram_raw = monopolar_fixed_point(points, constants)
+    ratio_of_one, ratio_after_measure = measure(N=constants.X*constants.Y, hologram=hologram_raw)
+    hologram_rand = random_hologram(ratio_of_one=ratio_of_one, hologram=hologram_raw, constants=constants)
+
 
     end = time.time()
     print(print("Cal time:{} sec".format(end - start)))
     print("CGH Calculation completed!")
 
     print("Preparing for display...")
-    show(hologram, constants.X, constants.Y)
+    show(hologram_raw, constants.X, constants.Y)
+    show(hologram_rand, constants.X, constants.Y)
+    # 2枚並べて表示
 
 
 if __name__ == "__main__":
