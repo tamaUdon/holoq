f9a9a46b7643f757df0a9a497dc3ae39b90b07a5
tamaUdon <sakurayuki603@gmail.com>
2026-04-09 18:15:30 +0900

    Fix - 四捨五入の計算の閾値を調整 / 50%に分けられることを確認 / サブ関数に分割

--- sample/monoq.py
+++ sample/monoq.py
@@ -1,19 +1,88 @@
 import time
-from decimal import ROUND_HALF_UP, Decimal, getcontext
+from decimal import (
+    Decimal,
+    getcontext,
+)
+from fractions import Fraction
 
 import numpy as np
 import tqdm
 from constants import ClassicalConstants
 from pointcloud import create_single_point, show
 
-# numpy を固定小数モードに
+### === Settings === ###
+DEBUG = False
+# numpy 固定小数モード
 np.set_printoptions(precision=16, floatmode="fixed", suppress=False)
 getcontext().prec = 16
+np.set_printoptions(threshold=np.inf)  # type: ignore # ログを全て出す
+### ================ ###
+
+
+def _print_probabilities_unique_value(array: np.ndarray, name: str):
+    """
+    Numpy配列内の要素数をカウントし、出現確率をprintする
+    """
+    values, counts = np.unique(array, return_counts=True)
+    probabilities = counts / array.size
+    print(f"{name}の統計情報")
+    for v, c, p in zip(values, counts, probabilities):
+        print(f"要素: {v}, カウント: {c}, 確率: {p:.2f} \n")
+
+
+def _extract_frac_part_from_theta(θ: np.ndarray) -> np.ndarray:
+    """
+    θ=M*N から小数部を取り出す
+    - 整数部と小数部に分ける
+    - 小数部を抜き出す
+    - decimal型に変換
+
+    return: 小数部<Decimal>
+    """
+
+    frac_part, int_part = np.modf(θ)  # 例) 1.5 -> (0.5, 1.0)
+    decimal_arr = np.array(  # Decimal型に変換し、.以下をstrとして格納
+        [[Decimal(str(x).split(".")[1]) for x in row] for row in frac_part],
+        dtype=object,
+    )
+    return decimal_arr
+
+
+def target(decimal_arr: np.ndarray) -> np.ndarray:
+    """
+    関数T(・)の実装
+    - Decimal型の小数部を受け取る
+    -
+    """
+
+    decimal_t_array = np.array(
+        [[(Decimal(str(int(x))[0])) for x in row] for row in decimal_arr],
+        dtype=int,
+    )
+    decimal_choice = np.where(
+        decimal_t_array <= 4,
+        0,  # 4以下を0に
+        np.where(decimal_t_array >= 6, 1, 5),  # 6以上を1に, 5はそのまま
+    )
+    mask_5 = decimal_choice == 5  # 5の部分を特定
+    count_5 = np.sum(mask_5)  # 5の数を数える
+    # 5 -> 0 or 1どちらかに振り分ける
+    decimal_choice[mask_5] = np.random.choice(
+        [0, 1],
+        size=count_5,
+        p=[0.5, 0.5],
+    )
+    _print_probabilities_unique_value(decimal_t_array, "decimal_t_array")
+    _print_probabilities_unique_value(decimal_choice, "decimal_choice")
+    return decimal_choice
 
 
 def monopolar_fixed_point(
     points: np.ndarray, constants: ClassicalConstants
 ) -> np.ndarray:
+    """
+    monopolar hologramの実装
+    """
     # 1. fixed-point monopolar generated holography
 
     x = np.arange(constants.X, dtype=np.int64)
@@ -25,76 +94,46 @@ def monopolar_fixed_point(
 
     p_sq = 2 * np.pi * constants.pp * constants.pp
     p_denom = constants.λ * constants.d
+    N = p_sq / p_denom  # noqa: N806
+    print(f"{p_sq=}, {p_denom=}, {N=}")
 
-    N = p_sq / p_denom  # N-bit 固定値なのでループの外側に出す
-    print(f"{p_sq=}")
-    print(f"{p_denom=}")
-    print(f"{N=}")
-
-    for i, (xj, yj, zj) in enumerate(tqdm.tqdm(points)):
+    for xj, yj, _ in tqdm.tqdm(points):
         xhj = xh.astype(np.int32) - xj  # hologram面を一気に計算
         yhj = yh.astype(np.int32) - yj
-        print(f"{xj=}, {yj=}, {zj=}")
-        print(f"{xhj=}")
-        print(f"{yhj=}")
-
         x_sq = xhj * xhj
         y_sq = yhj * yhj
-        M = x_sq + y_sq  # M-bit
+        M = x_sq + y_sq  # M-bit # noqa: N806
+        θ = M * N
+        print(f"{M=} , {θ=}")
 
-        print(f"{x_sq=}")
-        print(f"{y_sq=}")
-        print(f"{M=}")
-
-        θ = M * N  # θ
-        print(f"{θ=}")
-
-        frac_part, int_part = np.modf(θ)  # 例) 1.5 -> (0.5, 1.0)
-        print(f"{frac_part=}")
-        print(f"{int_part=}")
-
-        decimal_arr = np.array(
-            [
-                [Decimal(str(x).split(".")[1]) for x in row]
-                for row in frac_part
-            ],
-            dtype=object,
-        )  # Decimal型に変換し、.以下をstrとして格納
-        hologram = np.array(
-            [
-                [
-                    (Decimal(str(int(x))[0]) / 10).quantize(
-                        Decimal("0"), ROUND_HALF_UP
-                    )
-                    for x in row
-                ]
-                for row in decimal_arr
-            ],
-            dtype=int,
-        )  # 不格好なのでなおす
-        # decimal型 -> 0~9 整数が入ることはわかっている　-> /10して四捨五入
-
-        print(f"{hologram=}")
-
+        decimal_arr = _extract_frac_part_from_theta(θ)
+        hologram = target(decimal_arr)
     return hologram
 
 
-def measure(N: int, hologram: np.ndarray) -> tuple[float, float]:
+def measure(N: int, hologram: np.ndarray) -> Fraction:  # noqa: N803
+    """
+    1をカウントし、ホログラム内の1の割合を返却する
+    """
     count_one = np.count_nonzero(hologram == 1)
-    ratio_of_one = (1 / N) * count_one
-    ratio_after_measure = 1 / np.sqrt(N) / np.sqrt(ratio_of_one)
+    ratio_of_one = Fraction(count_one.item(), N)
+    print(f"{count_one=}, {ratio_of_one=}")
 
-    print(f"{ratio_of_one=}")  # 0.5569496154785156
-    print(f"{ratio_after_measure=}")
-    return ratio_of_one, ratio_after_measure
+    return ratio_of_one
 
 
 def random_hologram(
-    ratio_of_one: float, hologram: np.ndarray, constants: ClassicalConstants
+    ratio_of_one: Fraction,
+    hologram: np.ndarray,
+    constants: ClassicalConstants,
 ) -> np.ndarray:
+    """
+    measureの結果を元にホログラムのピクセルを1か0にフィルターする
+    """
     rng = np.random.default_rng()
     random_filter = rng.random((constants.X, constants.Y))
-    bool_filter = random_filter <= ratio_of_one
+    bool_filter = random_filter <= float(ratio_of_one)
+
     return bool_filter & hologram
 
 
@@ -104,9 +143,8 @@ def main():
     constants = ClassicalConstants()
     points = create_single_point(constants)
     hologram_raw = monopolar_fixed_point(points, constants)
-    ratio_of_one, ratio_after_measure = measure(
-        N=constants.X * constants.Y, hologram=hologram_raw
-    )
+    ratio_of_one = measure(N=constants.X * constants.Y, hologram=hologram_raw)
+
     hologram_rand = random_hologram(
         ratio_of_one=ratio_of_one, hologram=hologram_raw, constants=constants
     )
@@ -122,3 +160,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# TODO - bin() を使って計算ができるようにする
+# decimal型 -> 1~9 の整数が入ることはわかっている
+#   -> 1~4...0, 6~9...1, 5 ... 0|1半々にする random()
